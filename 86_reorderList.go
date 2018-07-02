2979d094f193817829968782c37e062c5456b4d6
guoxiaodong13@jd.com <g_uoxiaodong@163.com>
2018-07-02 20:44:40 +0800

    86

--- 86_reorderList.go
+++ 86_reorderList.go
@@ -8,10 +8,13 @@ type ListNode struct {
 }
 
 func reorderList(head *ListNode) {
+	if head == nil || head.Next == nil {
+		return
+	}
 	p1 := head
 	p2 := head.Next
-	for p2.Next != nil && p2 != nil {
-		fmt.Println("dubug p2.Val => %d", p2.Val)
+	for p2 != nil && p2.Next != nil {
+		//fmt.Println("dubug p2.Val => %d", p2.Val)
 		p1 = p1.Next
 		p2 = p2.Next.Next
 	}
@@ -33,6 +36,7 @@ func reorderList(head *ListNode) {
 	p2 = head2
 	n1 := head
 	n2 := head2
+
 	for p1 != nil && p2 != nil {
 		n1 = p1.Next
 		n2 = p2.Next
@@ -52,19 +56,19 @@ func main() {
 	l3 := new(ListNode)
 	l4 := new(ListNode)
 	l5 := new(ListNode)
-	l6 := new(ListNode)
+	// l6 := new(ListNode)
 	l1.Val = 1
 	l2.Val = 2
 	l3.Val = 3
 	l4.Val = 4
 	l5.Val = 5
-	l6.Val = 6
+	// l6.Val = 6
 	l1.Next = l2
 	l2.Next = l3
 	l3.Next = l4
 	l4.Next = l5
-	l5.Next = l6
-	l6.Next = nil
+	l5.Next = nil
+	// l6.Next = nil
 	reorderList(l1)
 	node := l1
 	for node != nil {
