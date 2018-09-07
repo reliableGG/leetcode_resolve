afc5b8739c3e6c6c8288bb9712a30eb27315ab0d
guoxiaodong13@jd.com <g_uoxiaodong@163.com>
2018-09-07 17:58:04 +0800

    450 fix

--- dev/tree/450_DeleteNodeinBST.go
+++ dev/tree/450_DeleteNodeinBST.go
@@ -9,10 +9,6 @@ type TreeNode struct {
 }
 
 func deleteNode(root *TreeNode, key int) *TreeNode {
-	if root == nil {
-		return nil
-	}
-
 	t, par := findNode(root, key)
 	//fmt.Println(t, par)
 	if t == nil {
@@ -20,12 +16,12 @@ func deleteNode(root *TreeNode, key int) *TreeNode {
 	}
 	if t == root {
 		return merge(t)
+	}
+
+	if par.Left == t {
+		par.Left = merge(t)
 	} else {
-		if par.Left == t {
-			par.Left = merge(t)
-		} else {
-			par.Right = merge(t)
-		}
+		par.Right = merge(t)
 	}
 	return root
 }
@@ -34,16 +30,19 @@ func findNode(root *TreeNode, key int) (t, par *TreeNode) {
 	par = nil
 	t = root
 
+	fmt.Println(key)
 	for t != nil {
-		//fmt.Println("t.Val =>", t.Val)
+		fmt.Println("t !=nil ", t)
 		if t.Val == key {
 			return t, par
 		}
-		if t.Val > key {
+		if t != nil && t.Val > key {
+			fmt.Println(">")
+			fmt.Println(t.Val)
 			par = t
 			t = t.Left
 		}
-		if root.Val < key {
+		if t != nil && t.Val < key {
 			par = t
 			t = t.Right
 		}
@@ -52,8 +51,14 @@ func findNode(root *TreeNode, key int) (t, par *TreeNode) {
 }
 
 func merge(t *TreeNode) *TreeNode {
-	if t == nil {
+	if t == nil || t.Left == nil && t.Right == nil {
 		return nil
+	}
+	if t.Left == nil {
+		return t.Right
+	}
+	if t.Right == nil {
+		return t.Left
 	}
 	tr := t.Right
 	for tr.Left != nil {
@@ -85,12 +90,30 @@ func main() {
 	n6.Left = nil
 	n6.Right = nil
 
-	n10 := TreeNode{Val: 0}
+	/*
+	 *    n1 := TreeNode{Val: 3}
+	 *    n2 := TreeNode{Val: 1}
+	 *    n3 := TreeNode{Val: 2}
+	 *    n4 := TreeNode{Val: 4}
+	 *
+	 *    n1.Left = &n2
+	 *    n1.Right = &n4
+	 *    n2.Left = nil
+	 *    n2.Right = &n3
+	 *    n3.Left = nil
+	 *    n3.Right = nil
+	 *    n4.Left = nil
+	 *    n4.Right = nil
+	 */
 
-	fmt.Println(n1)
-	node := deleteNode(&n10, 0)
+	//n10 := TreeNode{Val: 0}
+
+	//fmt.Println(n1)
+	node := deleteNode(&n1, 0)
 	fmt.Println(node)
+
 	fmt.Println(node.Left)
 	fmt.Println(node.Left.Left)
 	fmt.Println(node.Right)
+
 }
